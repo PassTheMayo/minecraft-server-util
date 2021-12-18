6100e720b1501ec2718b04bea3ccc2eb6a1203cd
Jacob Gunther <jacobgunther03+github@gmail.com>
2021-12-18 14:08:11 -0600

    Add isConnected property and timeouts to RCON

--- src/structure/RCON.ts
+++ src/structure/RCON.ts
@@ -6,6 +6,10 @@ import TCPClient from './TCPClient';
 
 const encoder = new TextEncoder();
 
+export interface RCONLoginOptions {
+	timeout?: number
+}
+
 export interface Message {
 	requestID: number,
 	message: string
@@ -19,12 +23,16 @@ export interface RCONEvents {
 }
 
 class RCON extends EventEmitter2 implements RCONEvents {
+	public isLoggedIn = false;
 	private socket: TCPClient | null = null;
 	private requestID = 0;
-	private isLoggedIn = false;
 
 	constructor() {
 		super();
+	}
+
+	get isConnected() {
+		return this.socket && this.socket.isConnected || false;
 	}
 
 	connect(host: string, port = 25575, options: Partial<NetConnectOpts> = {}): Promise<void> {
@@ -36,57 +44,88 @@ class RCON extends EventEmitter2 implements RCONEvents {
 		assert(port <= 65535, `Expected 'port' to be less than or equal to 65535, got '${port}'`);
 		assert(typeof options === 'object', `Expected 'options' to be an 'object', got '${typeof options}'`);
 
-		this.socket = new TCPClient();
+		return new Promise((resolve, reject) => {
+			this.socket = new TCPClient();
 
-		return this.socket.connect({ host, port, ...options });
+			const timeout = setTimeout(() => {
+				reject(new Error('Failed to connect to RCON server within timeout duration'));
+
+				this.socket?.close();
+			}, options?.timeout ?? 1000 * 5);
+
+			this.socket.connect({ host, port, ...options })
+				.then(() => {
+					clearTimeout(timeout);
+
+					resolve();
+				})
+				.catch((error) => {
+					clearTimeout(timeout);
+
+					reject(error);
+				});
+		});
 	}
 
-	async login(password: string): Promise<void> {
+	login(password: string, options: RCONLoginOptions = {}): Promise<void> {
 		assert(typeof password === 'string', `Expected 'password' to be a 'string', got '${typeof password}'`);
 		assert(password.length > 1, `Expected 'password' to have a length greater than 0, got ${password.length}`);
+		assert(typeof options === 'object', `Expected 'options' to be an 'object', got '${typeof options}'`);
 
-		if (this.socket === null || !this.socket.isConnected) throw new Error('login() attempted before RCON has connected');
+		return new Promise(async (resolve, reject) => {
+			if (this.socket === null || !this.socket.isConnected) return reject(new Error('login() attempted before RCON has connected'));
 
-		this.isLoggedIn = false;
+			const timeout = setTimeout(() => {
+				reject(new Error('Failed to connect to RCON server within timeout duration'));
 
-		const passwordBytes = encoder.encode(password);
+				this.socket?.close();
+			}, options?.timeout ?? 1000 * 5);
 
-		// Login packet
-		// https://wiki.vg/RCON#3:_Login
-		{
-			this.socket.writeInt32LE(10 + passwordBytes.byteLength);
-			this.socket.writeInt32LE(this.requestID++);
-			this.socket.writeInt32LE(3);
-			this.socket.writeBytes(passwordBytes);
-			this.socket.writeBytes(Uint8Array.from([0x00, 0x00]));
-			await this.socket.flush(false);
-		}
+			this.isLoggedIn = false;
 
-		// Login response packet
-		// https://wiki.vg/RCON#3:_Login
-		{
-			await this.socket.readInt32LE();
+			const passwordBytes = encoder.encode(password);
 
-			const requestID = await this.socket.readInt32LE();
-			if (requestID === -1) throw new Error('Invalid RCON password');
-
-			const packetType = await this.socket.readInt32LE();
-			if (packetType !== 2) throw new Error('Expected server to send packet type 2, received ' + packetType);
-
-			await this.socket.readBytes(2);
-		}
-
-		process.nextTick(async () => {
-			while (this.socket !== null && this.socket.isConnected && this.isLoggedIn) {
-				try {
-					await this._readPacket();
-				} catch {
-					// Ignore
-				}
+			// Login packet
+			// https://wiki.vg/RCON#3:_Login
+			{
+				this.socket.writeInt32LE(10 + passwordBytes.byteLength);
+				this.socket.writeInt32LE(this.requestID++);
+				this.socket.writeInt32LE(3);
+				this.socket.writeBytes(passwordBytes);
+				this.socket.writeBytes(Uint8Array.from([0x00, 0x00]));
+				await this.socket.flush(false);
 			}
-		});
 
-		this.isLoggedIn = true;
+			// Login response packet
+			// https://wiki.vg/RCON#3:_Login
+			{
+				await this.socket.readInt32LE();
+
+				const requestID = await this.socket.readInt32LE();
+				if (requestID === -1) throw new Error('Invalid RCON password');
+
+				const packetType = await this.socket.readInt32LE();
+				if (packetType !== 2) throw new Error('Expected server to send packet type 2, received ' + packetType);
+
+				await this.socket.readBytes(2);
+			}
+
+			this.isLoggedIn = true;
+
+			clearTimeout(timeout);
+
+			resolve();
+
+			process.nextTick(async () => {
+				while (this.socket !== null && this.socket.isConnected && this.isLoggedIn) {
+					try {
+						await this._readPacket();
+					} catch (e) {
+						this.emit('error', e);
+					}
+				}
+			});
+		});
 	}
 
 	async run(command: string): Promise<number> {
@@ -147,9 +186,7 @@ class RCON extends EventEmitter2 implements RCONEvents {
 	}
 
 	close(): void {
-		if (!this.socket) return;
-
-		this.socket.close();
+		this.socket?.close();
 	}
 }
 
